2f9b84d10739d747131e9a8ee255e90664054672
Dilaver Şerif <116176715+DilaverMai@users.noreply.github.com>
2023-02-02 17:26:51 +0300

    disfsdf

---
+++ Assets/Character/Attacker.cs
@@ -0,0 +1,29 @@
+using Character;
+using UnityEngine.Events;
+
+namespace Character
+{
+    [System.Serializable]
+    public class Attacker : IAttackable,IInitializable
+    {
+        public AttackerData attackerData;
+        public UnityAction OnAttack;
+        
+        public void Attack(Health health)
+        {
+            health.TakeDamage(ref health,attackerData.Damage);
+            OnAttack?.Invoke();
+        }
+
+        public void Initialize()
+        {
+            
+        }
+        
+    }
+}
+
+public interface IAttackable
+{
+    void Attack(Health health);
+}
---
+++ Assets/Character/StandartEnemy.cs
@@ -0,0 +1,22 @@
+namespace Character
+{
+    public class StandartEnemy : EnemyBase
+    {
+        public Health HealthSystem;
+        public Attacker AttackSystem;
+        public CharacterAnimation<EnemyAnimationList> AnimationSystem;
+        public CharacterMoveWithNavMesh MoveSystem;
+        
+        public override void OnSpawn()
+        {
+            HealthSystem.Initialize();
+
+            HealthSystem.OnDeath += () => AnimationSystem.PlayAnimation(EnemyAnimationList.Death);
+        }
+        
+        public override void OnDeath()
+        {
+            HealthSystem.OnDeath -= () => AnimationSystem.PlayAnimation(EnemyAnimationList.Death);
+        }
+    }
+}
